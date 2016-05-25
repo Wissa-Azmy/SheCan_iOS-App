16e2b3af460d456b91bc5b60e9a8834d49788e66
wissa-azmy <wissa.azmy@gmail.com>
2016-05-25 20:05:46 +0200

    First Commit. The Login Page created

--- SheCan/ViewController.swift
+++ SheCan/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+
+    func displayAlert (title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // To Dismiss the keyboard when Tapping anywhere on the screen
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
+        view.addGestureRecognizer(tap)
+        
+        // Delegate Fields that will activate the hide Keyboard function when return key touched
+        self.usernameField.delegate = self
+        self.passwordField.delegate = self
+    }
+    
+    // This func is called by the Tap constant above when the screen is tapped.
+    func DismissKeyboard() {
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func LoginBtn(sender: AnyObject) {
+        if usernameField.text == "" || passwordField.text == "" {
+            displayAlert("Missing Field(s)", message: "Username and password are required")
+        }
+    }
 
+    @IBAction func signupBtn(sender: AnyObject) {
+    }
 }
 
